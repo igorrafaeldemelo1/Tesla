960077c8e002921447e40dd3c5ea2ca7f2f4046f
igorrafaeldemelo1 <86748756+igorrafaeldemelo1@users.noreply.github.com>
2022-12-12 23:05:24 -0300

    att 3

--- js/java.js
+++ js/java.js
@@ -1,1 +1,53 @@
+function pageDown(){
+    document.getElementById('model').innerHTML = "Model Y";
+    
+}
+function pageDown01(){
+    document.getElementById('model').innerHTML = "Model S";
+    
+}
+function pageDown02(){
+    document.getElementById('model').innerHTML = "Model X";
+    
+}
+function pageDown03(){
+    document.getElementById('model').innerHTML = "Solar Panels";
+    
+}
+function pageDown04(){
+    document.getElementById('model').innerHTML = "Solar Roof";
+    
+}
+function pageDown05(){
+    document.getElementById('model').innerHTML = "Accessories";
+    
+}
+function pageup(){
+    document.getElementById('model').innerHTML = "Accessories";
+    
+}
+function pageup01(){
+    document.getElementById('model').innerHTML = "Model 3";
+    
+}
+function pageup02(){
+    document.getElementById('model').innerHTML = "Model Y";
+    
+}
+function pageup03(){
+    document.getElementById('model').innerHTML = "Model S";
+    
+}
+function pageup04(){
+    document.getElementById('model').innerHTML = "Model X";
+    
+}
+function pageup05(){
+    document.getElementById('model').innerHTML = "Solar Panels";
+    
+}
+function pageup06(){
+    document.getElementById('model').innerHTML = "Solar Roof";
+    
+}
 
